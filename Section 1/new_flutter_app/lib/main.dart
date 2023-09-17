498f9c991761c595809cf0ab5d44b022a7463397
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-09-17 20:02:00 -0300

    Added constructor to widget

--- Section 1/new_flutter_app/lib/main.dart
+++ Section 1/new_flutter_app/lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     home: Scaffold(body: GradientContainer()),
   ));
 }
 
 class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
   @override
   Widget build(context) {
     return Container(
