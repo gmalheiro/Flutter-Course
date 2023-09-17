94bafcffcec1c1d15c73d93d38a71553a8ea8176
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-09-17 19:36:22 -0300

    Added linear gradient direction

--- Section 1/new_flutter_app/lib/main.dart
+++ Section 1/new_flutter_app/lib/main.dart
@@ -8,7 +8,7 @@ void main() {
             gradient: LinearGradient(colors: [
           Color.fromARGB(255, 26, 2, 80),
           Color.fromARGB(255, 45, 7, 98)
-        ])),
+        ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
         child: const Center(
           child: Text("Hello World!"),
         ),
