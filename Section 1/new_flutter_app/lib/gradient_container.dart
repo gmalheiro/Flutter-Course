2169052bf5d408437cd81afbeaa3fffa0a41353c
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-09-18 10:09:43 -0300

    Created variables

--- Section 1/new_flutter_app/lib/gradient_container.dart
+++ Section 1/new_flutter_app/lib/gradient_container.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+
 import 'package:new_flutter_app/styled_text.dart';
+
+var startAlignment = Alignment.topLeft;
+var endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
@@ -11,7 +15,7 @@ class GradientContainer extends StatelessWidget {
           gradient: LinearGradient(colors: [
         Color.fromARGB(255, 26, 2, 80),
         Color.fromARGB(255, 45, 7, 98)
-      ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+      ], begin: startAlignment, end: endAlignment)),
       child: const Center(child: StyledText()),
     );
   }
